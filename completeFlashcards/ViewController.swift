91b55ae9e1b8481e21c69daf0d6f4674c440325e
Taia Williams-Rivera <tmwilliams181@students.desu.edu>
2020-06-16 17:35:40 -0400

    completed several optional steps

--- completeFlashcards/ViewController.swift
+++ completeFlashcards/ViewController.swift
@@ -10,18 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var option1: UIButton!
+    @IBOutlet weak var option2: UIButton!
+    @IBOutlet weak var option3: UIButton!
+    @IBOutlet weak var card: UIView!
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        frontLabel.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        card.layer.cornerRadius = 20.0
+        
+        option1.layer.cornerRadius = 20.0
+        option2.layer.cornerRadius = 20.0
+        option3.layer.cornerRadius = 20.0
+        
+        option1.layer.borderWidth = 3.0
+        option2.layer.borderWidth = 3.0
+        option3.layer.borderWidth = 3.0
+        
+        option1.layer.borderColor = #colorLiteral(red: 1, green: 0.9666234854, blue: 0.3041960932, alpha: 1)
+        option2.layer.borderColor = #colorLiteral(red: 1, green: 0.9666234854, blue: 0.3041960932, alpha: 1)
+        option3.layer.borderColor = #colorLiteral(red: 1, green: 0.9666234854, blue: 0.3041960932, alpha: 1)
+
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        
+        card.layer.shadowRadius = 20.0
+        card.layer.shadowOpacity = 1.0
     }
     
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         frontLabel.isHidden = true;
     }
     
-
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        option1.isHidden = true
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+    }
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        option3.isHidden = true;
+    }
+    
 }
 
